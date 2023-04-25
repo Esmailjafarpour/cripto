26a852a1e038c05eb807375507dd919fa92f72a0
nader <esmailjafarpour29@gmail.com>
2023-04-25 18:54:33 +0330

    create componenet coin & handle search

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,32 +3,58 @@ import React,{useState,useEffect} from 'react';
 //Api
 import {getCoin} from "../services/api";
 
+//component
+import Loader from '../components/Loader';
+import Coin from '../components/Coin'
+
 const Landing = () => {
 
-     const [state, setState] = useState([]);
+     const [coins, setCoins] = useState([]);
+     const [search, setSearch] = useState("");
+
 
      useEffect(() => {
 
           const fetchApi = async () => {
                const data = await getCoin();
-               console.log(data)
-               setState(data)
+               setCoins(data)
           }
 
           fetchApi()
          
      }, []);
 
+     const searchHandler = (event) => {
+          setSearch(event.target.value)
+     }
+
+     const searchCoins = coins.filter( coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
      return (
 
-          <div>
-               <ul>
-                   {state.map((item)=>{
-                         <li key={item.id}>{item.id}</li>
-                   })} 
-               </ul>
+          <>
+               <input type="text" placeholder="Search" value={search} onChange={searchHandler}/>
                
-          </div>
+               {coins.length ? 
+                    <div>
+                         {searchCoins.map(coin =>(
+                              <Coin 
+                                   key={coin.id}
+                                   symbol={coin.symbol}
+                                   name={coin.name}
+                                   image={coin.image}
+                                   price={coin.current_price}
+                                   marketCap={coin.market_cap}
+                                   priceChange={coin.price_change_percentage_24h}
+                              />
+                         ))}
+                    </div>
+               :
+                   <Loader/> 
+               }
+              
+              
+          </>
 
      );
 }
